a0ec246f9b854f3c4cd363a3585a528faa77ce84
Jay Oliver <jaykamas@gmail.com>
2020-07-22 16:44:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ def game_hash
     :home => {:team_name => "Brooklyn Nets",
               :colors => ["Black", "White"],
               :players => [
-                {:player_name => ["Alan Anderson"]},
+                {:player_name => "Alan Anderson"},
                 {:number => [0, 30, 11, 1, 31]}
                 ]
     },
